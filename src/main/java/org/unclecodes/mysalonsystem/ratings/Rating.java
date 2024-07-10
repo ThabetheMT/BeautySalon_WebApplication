375c4cfe8c2a1659b1fc6a1f0ed55676d0b123d0
222249724 <222249724@tut4life.ac.za>
2024-07-10 13:24:03 +0200

    adding ratings features

--- src/main/java/org/unclecodes/mysalonsystem/ratings/Rating.java
+++ src/main/java/org/unclecodes/mysalonsystem/ratings/Rating.java
@@ -1,6 +1,52 @@
 package org.unclecodes.mysalonsystem.ratings;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.unclecodes.mysalonsystem.client.Client;
+import org.unclecodes.mysalonsystem.stylist.Stylist;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Rating {
 
+    @Id
+    @SequenceGenerator(
+            name = "sequence_rating",
+            sequenceName = "sequence_rating",
+            allocationSize = 1,
+            initialValue = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "sequence_rating"
+    )
     private Integer id;
+    @Column(
+            length = 1
+    )
+    private int rate;
+    @ManyToOne(
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            referencedColumnName = "email"
+    )
+    private Client client;
+    @ManyToOne(
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            referencedColumnName = "email"
+    )
+    private Stylist stylist;
+    private Date creationDate;
+
 }
